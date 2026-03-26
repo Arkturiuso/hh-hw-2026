05a6801a07f1a764dad4b31c011ee6b34bf499fd
Kirill Tagai <tagaykirill83@gmail.com>
2026-03-27 01:54:02 +0500

    Added logic  and tests

--- app/switchboard.py
+++ app/switchboard.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from app.users import User
+from app.users import User, LocalUser, ForeignUser
 
 
 LOCAL_PHONE_PREFIX = "+7"
@@ -21,6 +21,15 @@ class ActiveCall:
 class Switchboard:
     def __init__(self) -> None:
         self._active_calls: list[ActiveCall] = []
+        self._cross_border_calls_count: int = 0
+
+    @staticmethod
+    def strip_all(*args: str) -> tuple[str, ...]:
+        return tuple(arg.strip() for arg in args)
+
+    @staticmethod
+    def is_local_phone_number(phone_number: str) -> bool:
+        return phone_number.startswith(LOCAL_PHONE_PREFIX)
 
     def register_call(self, raw_call: str) -> ActiveCall:
         '''
@@ -29,10 +38,32 @@ class Switchboard:
 
         Например: "1001,Иван Петров,+71234567890,1085,Адам Яковлев,+71255556666"
         '''
-        pass  # Удалите `pass` и пишите ваш код
+        raw_parts = raw_call.split(',')
+        cleaned_parts = self.strip_all(*raw_parts)
+
+        caller_parts = cleaned_parts[:3]
+        receiver_parts = cleaned_parts[3:]
+
+        caller = self.create_user(*caller_parts)
+        receiver = self.create_user(*receiver_parts)
+        
+        active_call = ActiveCall(caller, receiver)
+        self._active_calls.append(active_call)
+
+        if active_call.is_cross_border:
+            self._cross_border_calls_count += 1
+
+        return active_call
+    
+    def create_user(self, user_id: str, user_fullname: str, user_phone_number: str) -> User:
+        user_id = int(user_id)
+        if self.is_local_phone_number(user_phone_number):
+            return LocalUser(user_id, user_fullname, user_phone_number)
+        return ForeignUser(user_id, user_fullname, user_phone_number)
 
     def get_active_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return len(self._active_calls)
 
     def get_cross_border_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return self._cross_border_calls_count
+
--- tests/test_switchboard.py
+++ tests/test_switchboard.py
@@ -45,3 +45,62 @@ def test_register_call_counts_calls_between_local_and_foreign_users() -> None:
 
     assert switchboard.get_active_calls_count() == 3
     assert switchboard.get_cross_border_calls_count() == 1
+
+
+def test_create_user_converts_id_to_int() -> None:
+    switchboard = Switchboard()
+    
+    user = switchboard.create_user("3", "Pedro Grunge" , "+70009998887")
+    
+    assert isinstance(user.id, int)
+    assert user.id == 3
+
+
+def test_create_user_returns_correct_type() -> None:
+    switchboard = Switchboard()
+    
+    first_user = switchboard.create_user("1", "Alice Laren", "+79990000000")
+    assert isinstance(first_user, LocalUser)
+    
+    second_user = switchboard.create_user("2", "Gilbert North", "+22233344455")
+    assert isinstance(second_user, ForeignUser)
+
+
+def test_caller_and_receiver_phone_location() -> None:
+    switchboard = Switchboard()
+
+    new_call = switchboard.register_call(
+        "4,Charlie Green,+70009998887,5,John Pork,+488997766554"
+    )
+
+    assert switchboard.is_local_phone_number(new_call.caller.phone)
+    assert not switchboard.is_local_phone_number(new_call.receiver.phone)
+
+
+def test_caller_and_receiver_data_correctly_processed() -> None:
+    switchboard = Switchboard()
+
+    new_call = switchboard.register_call(
+        "1,Jack Sparrow,+71234567891,2,John Snow,+20987654321"
+    )
+
+    assert new_call.caller.id == 1
+    assert new_call.caller.fullname == 'Jack Sparrow'
+    assert new_call.caller.phone == '+71234567891'
+
+    assert new_call.receiver.id == 2
+    assert new_call.receiver.fullname == 'John Snow'
+    assert new_call.receiver.phone == '+20987654321'
+
+    assert new_call.caller is not new_call.receiver
+
+
+def test_register_call_with_spaces_in_names() -> None:
+    switchboard = Switchboard()
+    
+    new_call = switchboard.register_call(
+        "2,     Charlie Green   ,+70009998887,3,  John Pork     ,+488997766554"
+    )
+    
+    assert new_call.caller.fullname == "Charlie Green"
+    assert new_call.receiver.fullname == "John Pork"
